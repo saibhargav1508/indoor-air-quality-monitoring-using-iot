6e326a4fa0ee84e1a0d1d3502a83cfbfe4c303b4
Sai Bhargav Mandavilli <saibhargav1508@gmail.com>
2021-12-19 16:48:35 -0500

    Update cloud_react_analysis.m

--- cloud_react_analysis.m
+++ cloud_react_analysis.m
@@ -1,6 +1,11 @@
+% This code is added to the MATLAB analysis in ThingSpeak and is triggered by React.
+% code sends an email whenever the readings meet the react condition
+
 alert_body = 'The PM2 dust sensor has detected a reading above the set threshold';
 alert_subject = 'Dust sensor reading above threshold';
-alert_api_key = 'TAKM642RV1WZ0QNM1PGJ4';
+
+alert_api_key = '*****'; % add your channel alert API key here
+
 alert_url= "https://api.thingspeak.com/alerts/send";
 jsonmessage = sprintf(['{"subject": "%s", "body": "%s"}'], alert_subject,alert_body);
 options = weboptions("HeaderFields", {'Thingspeak-Alerts-API-Key', alert_api_key; 'Content-Type','application/json'});
